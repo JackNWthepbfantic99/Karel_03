4acdfafe9685f5f24ad69662a956a15eb25be03d
JackNWthepbfantic99 <staceynavin@Jacks-Air-2.default>
2014-11-03 17:09:24 +0000

    Jack and Tenzing got the treasure

--- Lab03.java
+++ Lab03.java
@@ -12,6 +12,41 @@ import edu.fcps.karel2.Robot;
 public class Lab03 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+     Display.openWorld("maps/mountain.map");
+     Display.setSize(16, 16);
+     Climber jack = new Climber(8);
+     Climber tenzing = new Climber(8);
+               jack.turnRight();
+               tenzing.turnRight();
+               jack.move();
+               tenzing.move();
+               jack.turnLeft();
+               tenzing.turnLeft();
+               jack.climbupRight();
+               tenzing.climbupRight();
+               jack.climbupRight();
+               tenzing.climbupRight();
+               jack.climbupRight();
+               tenzing.climbupRight();               
+               jack.climbdownRight();
+               tenzing.climbdownRight();
+               jack.climbdownRight();
+               tenzing.climbdownRight();
+               jack.pickBeeper();
+               jack.climbupLeft();
+               tenzing.climbupLeft();
+               jack.climbupLeft();
+               tenzing.climbupLeft();
+               jack.climbdownLeft();
+               tenzing.climbdownLeft();
+               jack.climbdownLeft();
+               tenzing.climbdownLeft();
+               jack.climbdownLeft();
+               tenzing.climbdownLeft();
+               jack.turnLeft();
+               tenzing.turnLeft();
+               jack.move();
+               tenzing.move();
+               
      }
-}
+ }
